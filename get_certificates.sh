f58854ba5ce765247dedc75e7f5f8b8d52fcc2c1
Joan Marc Carbo <jmcarbo@gmail.com>
2016-03-26 14:37:13 +0100

    ...

--- get_certificates.sh
+++ get_certificates.sh
@@ -4,4 +4,5 @@ certs=$(curl -s http://rancher-metadata/latest/self/container/labels/traefik.cer
 IFS=',' read -ra ADDR <<< "$certs"
 for i in ${ADDR[@]}; do
   echo "Getting $i"
+  curl -s -u "$RANCHER_ACCESS_KEY:$RANCHER_SECRET_KEY" http://192.168.99.100:8080/v1/projects/1a5/certificates| jq -r '.data[] | select(.name | . and contains("'"$i"'")) | .cert'
 done
