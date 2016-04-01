b461535f7b9a7be13d6465db6207ee363466885b
Joan Marc Carbo Arnau <joanmarc@gmail.com>
2016-04-01 12:00:18 +0200

    ...

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 export GODEBUG=netdns=cgo
+sleep 10
 gosuv add --name "confd" -- /usr/local/bin/confd -backend rancher -interval ${CONFD_INTERVAL:-15}
 gosuv add --name "traefik" -- /usr/local/bin/traefik -c /etc/traefik.toml
